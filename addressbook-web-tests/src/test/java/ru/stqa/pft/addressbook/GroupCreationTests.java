9055b29c1d8a586147bf73755ed8a89f4e76bb94
LinaOnoriz <linaonoriz@gmail.com>
2019-07-19 17:27:23 +0700

    Часть кода из теста, отвечающая за открытие страницы и логин, перенесена в Before-метод

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -18,17 +18,18 @@ public class GroupCreationTests {
     System.setProperty("webdriver.chrome.driver", "D:\\Dev\\QA_Lesson\\addressbook-web-tests\\geckodriver.exe");
     driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void GroupCreationTests() throws Exception {
     driver.get("http://localhost/addressbook/");
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])" +
+            "[1]/following::input[2]")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
